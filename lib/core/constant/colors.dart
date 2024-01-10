b18366960d0576dcca980295aabb90f991f1a2c2
Ahmad Nazzal <naz131681@gmail.com>
2024-01-10 15:35:06 +0200

    note model

--- lib/core/constant/colors.dart
+++ lib/core/constant/colors.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 
 abstract class ColorApp {
   static Color ColorList = const Color(0xff7C7C7C);
+
   static Color ColorContenerList = const Color(0xffE2E2E2);
+
   static Color ColorNote1 = const Color(0xffD9E8FC);
 
   static Color ColorNote2 = const Color(0xffFFD8F4);
